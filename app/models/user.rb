5e9790457d88cf9634d918e78cc11b0bd9454113
Clarence Williams <xwilliams.clarencex@gmail.com>
2021-02-09 02:26:12 +0000

    add user validation && min. password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
 
   has_secure_password
 
